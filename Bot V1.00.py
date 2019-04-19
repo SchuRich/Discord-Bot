7f0ad0ca2710dacca1202c5bf2d1e903fde675aa
SchuRich <49769313+SchuRich@users.noreply.github.com>
2019-04-19 04:55:47 +0200

    Update Bot V1.00.py

--- Bot V1.00.py
+++ Bot V1.00.py
@@ -1,92 +1,77 @@
+import discord
+from discord.ext import commands
+import Halp_py_test
+
+dwacription = "test"
+
+bot = commands.Bot(
+    command_prefix='!',
+    description="VFO comunity Bot",
+    case_insensitive=True,
+    pm_help=True)   #Not working
+
+bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+    print("Loged In")
+
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+
+@bot.event
+async def on_command_error(ctx, error):
+    ignored = (commands.CommandNotFound)
+    if isinstance(error, ignored):
+        print("Command Error Ignored")
+        return
 
 #Seek 0 Halp
+@bot.command()
 async def Halp(ctx):
-    await ctx.send(
-        "1-> !Links_Info\n"
-        "2-> !Game_Info\n"
-        "3-> !Update_Info\n"
-        "4-> !Staff_Info\n"
-        "5-> !Servers_List\n"
-        "6-> !Quest_Info **X**\n"
-        "7-> !Brutals_Info **X**\n"
-        "8-> !Might_Info **X**\n"
-        "9-> !Reborn_Info **X**\n")
+    await Halp_py_test.Halp(ctx)
 
-#Seek 1 Commands_Info
+#Seek 1 Links_Info
+@bot.command()
 async def Links_Info(ctx):
-    await ctx.send("https://vampiresfallorigins.gamepedia.com/Vampire%27s_Fall:_Origins\n"
-                   "https://www.reddit.com/r/vampiresfallorigins\n"
-                   "https://www.facebook.com/vampiresfall/")
-
+    await Halp_py_test.Links_Info(ctx)
 
 #Seek 2 Game_Info
+@bot.command()
 async def Game_Info(ctx):
-    await ctx.send("**__Developer__** --> Early Morning Studio\n"
-        "**__Publisher__** --> Early Morning Studio\n"
-        "**__E-mail__** --> vampiresfall@gmail.com\n"
-        "**__Platforms__** --> Android / Apple\n"
-        "**__Release date__** --> September 8, 2018\n"
-        "**__Genre__** --> RPG\n"
-        "**__Mode__** --> Singleplayer (Offline) , Multiplayer\n\n"
-        "__Android__ - 97 MB Storage space / Android 4.1+\n"
-        "__Apple__ - 468.9 MB Storage space / Requires iOS 10.0+ / Compatible with iPhone, iPad, and iPod touch.")
+    await Halp_py_test.Game_Info(ctx)
 
 #Seek 3 Update_Info
+@bot.command()
 async def Update_Info(ctx):
-    await ctx.send("__**Update 1.1.2**__\n"
-                   "- NG+\n"
-                   "- Stun in PvP now reduces focus instead of properly stunning in combo turns\n"
-                   "- Item Augmentation\n"
-                   "- Raised level cap to 75\n"
-                   "- Ability to remove bloodstones from items\n"
-                   "- Creature Lure potions available in stores\n"
-                   "- Removed Armor Cap\n"
-                   "- Less effect from bloodstones for stun, block chance, crit. dmg and crit. hit\n"
-                   "- Displaying PvP-resilience values in PvP-menu\n"
-                   "- Added  shield block resilience in PvP\n"
-                   "- General bug fixes")
+    await Halp_py_test.Update_Info(ctx)
 
 #Seek 4 Staff_Info
+@bot.command()
 async def Staff_Info(ctx):
-    await ctx.send("**__Mods__**\n"
-                   "__SchuRich__ ( Comunity Menager )\n"
-                   "\n"
-                   "__Bloodshed72__\n"
-                   "__Dahblackwack__\n"
-                   "__Dorsaddi__\n"
-                   "__Kaesar__ ( Wiki edit )\n"
-                   "__Nyssa__\n"
-                   "__Thoreon__ ( Wiki Edit )\n"
-                   "__T12__\n"
-                   "__Vartiainen__\n")
+    await Halp_py_test.Staff_Info(ctx)
 
 #Seek 5 Servers_Info
+@bot.command()
 async def Servers_Info(ctx):
-    await ctx.send("**__Discord Server Administration__** Server Mods (Semi Server Mod) / Judges\n\n"
-                   "__Offical__ - Schurich\n"
-                   "__Beta__ - Vartiainen ( Aabris )\n"
-                   "__Clans__ - Dahblackwack\n"
-                   "__Events__ - Free / T12 , Dahblackwack \n"
-                   "__Ideas__ - Big Daddy Chungus\n"
-                   "__Memz__ - Dahblackwack ( Rumsholic )\n"
-                   "__Tournaments__ - Nyssa / bloodshed72 , Kaeser\n"
-                   "")
+    await Halp_py_test.Servers_Info(ctx)
+
 
 #Seek 6 Quests_Info
+@bot.command()
 async def Quests_Info(ctx):
-    await ctx.send("**__Not Updated__**")
+    await Halp_py_test.Quests_Info(ctx)
 
 #Seek 7 Brutals_Info
+@bot.command()
 async def Brutals_Info(ctx):
-    await ctx.send("To see info on brutal u need please use after Brutals_<Brutal Name>")
+    await Halp_py_test.Brutals_Info(ctx)
 
-#####
 
-#Seek Brutals Data
+@bot.command()
 async def Brutals_Enigma(ctx):
-    await ctx.send("**__Enigma__**\n"
-                   "__Location:__ Anywhere outside towns and towerns after completed quest.\n\n"
-                   "__Quest:__ Find all 5 Riddles spread throu map and after finishing them all go to eastheaven find \"Riddle Master\" and after gaining ammulet u will be able to find enigma at his location.")
+    await Halp_py_test.Brutals_Enigma(ctx)
 
 
 
@@ -97,17 +82,53 @@ async def Brutals_Enigma(ctx):
 
 
 
-
-
-
-
-
-#####
 
 #Seek 8 Mights_Info
+@bot.command()
 async def Mights_Info(ctx):
-    await ctx.send("**__Not Updated__**")
+    await Halp_py_test.Mights_Info(ctx)
 
 #Seek 9 Reborn_Info
+@bot.command()
 async def Reborn_Info(ctx):
-    await ctx.send("**__Not Updated__**")
+    await Halp_py_test.Reborn_Info(ctx)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
