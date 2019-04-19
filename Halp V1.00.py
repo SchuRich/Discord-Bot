3550a3514c58decfcd3f80cb0c5aadb356bfbf38
SchuRich <49769313+SchuRich@users.noreply.github.com>
2019-04-19 04:56:44 +0200

    Update Halp V1.00.py

--- Halp V1.00.py
+++ Halp V1.00.py
@@ -1,9 +1,29 @@
 
-# Get Game Info Command
-async def get_game_info(ctx):
+#Seek 0 Halp
+async def Halp(ctx):
     await ctx.send(
-        "**__Developer__** --> Early Morning Studio\n"
+        "1-> !Links_Info\n"
+        "2-> !Game_Info\n"
+        "3-> !Update_Info\n"
+        "4-> !Staff_Info\n"
+        "5-> !Servers_List\n"
+        "6-> !Quest_Info **X**\n"
+        "7-> !Brutals_Info **X**\n"
+        "8-> !Might_Info **X**\n"
+        "9-> !Reborn_Info **X**\n")
+
+#Seek 1 Commands_Info
+async def Links_Info(ctx):
+    await ctx.send("https://vampiresfallorigins.gamepedia.com/Vampire%27s_Fall:_Origins\n"
+                   "https://www.reddit.com/r/vampiresfallorigins\n"
+                   "https://www.facebook.com/vampiresfall/")
+
+
+#Seek 2 Game_Info
+async def Game_Info(ctx):
+    await ctx.send("**__Developer__** --> Early Morning Studio\n"
         "**__Publisher__** --> Early Morning Studio\n"
+        "**__E-mail__** --> vampiresfall@gmail.com\n"
         "**__Platforms__** --> Android / Apple\n"
         "**__Release date__** --> September 8, 2018\n"
         "**__Genre__** --> RPG\n"
@@ -11,8 +31,8 @@ async def get_game_info(ctx):
         "__Android__ - 97 MB Storage space / Android 4.1+\n"
         "__Apple__ - 468.9 MB Storage space / Requires iOS 10.0+ / Compatible with iPhone, iPad, and iPod touch.")
 
-
-async def get_update(ctx):
+#Seek 3 Update_Info
+async def Update_Info(ctx):
     await ctx.send("__**Update 1.1.2**__\n"
                    "- NG+\n"
                    "- Stun in PvP now reduces focus instead of properly stunning in combo turns\n"
@@ -25,3 +45,69 @@ async def get_update(ctx):
                    "- Displaying PvP-resilience values in PvP-menu\n"
                    "- Added  shield block resilience in PvP\n"
                    "- General bug fixes")
+
+#Seek 4 Staff_Info
+async def Staff_Info(ctx):
+    await ctx.send("**__Mods__**\n"
+                   "__SchuRich__ ( Comunity Menager )\n"
+                   "\n"
+                   "__Bloodshed72__\n"
+                   "__Dahblackwack__\n"
+                   "__Dorsaddi__\n"
+                   "__Kaesar__ ( Wiki edit )\n"
+                   "__Nyssa__\n"
+                   "__Thoreon__ ( Wiki Edit )\n"
+                   "__T12__\n"
+                   "__Vartiainen__\n")
+
+#Seek 5 Servers_Info
+async def Servers_Info(ctx):
+    await ctx.send("**__Discord Server Administration__** Server Mods (Semi Server Mod) / Judges\n\n"
+                   "__Offical__ - Schurich\n"
+                   "__Beta__ - Vartiainen ( Aabris )\n"
+                   "__Clans__ - Dahblackwack\n"
+                   "__Events__ - Free / T12 , Dahblackwack \n"
+                   "__Ideas__ - Big Daddy Chungus\n"
+                   "__Memz__ - Dahblackwack ( Rumsholic )\n"
+                   "__Tournaments__ - Nyssa / bloodshed72 , Kaeser\n"
+                   "")
+
+#Seek 6 Quests_Info
+async def Quests_Info(ctx):
+    await ctx.send("**__Not Updated__**")
+
+#Seek 7 Brutals_Info
+async def Brutals_Info(ctx):
+    await ctx.send("To see info on brutal u need please use after Brutals_<Brutal Name>")
+
+#####
+
+#Seek Brutals Data
+async def Brutals_Enigma(ctx):
+    await ctx.send("**__Enigma__**\n"
+                   "__Location:__ Anywhere outside towns and towerns after completed quest.\n\n"
+                   "__Quest:__ Find all 5 Riddles spread throu map and after finishing them all go to eastheaven find \"Riddle Master\" and after gaining ammulet u will be able to find enigma at his location.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#####
+
+#Seek 8 Mights_Info
+async def Mights_Info(ctx):
+    await ctx.send("**__Not Updated__**")
+
+#Seek 9 Reborn_Info
+async def Reborn_Info(ctx):
+    await ctx.send("**__Not Updated__**")
