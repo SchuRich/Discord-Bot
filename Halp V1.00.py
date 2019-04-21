e4e59f83bc8831af04e291c027cfe2ceaa94f7e9
SchuRich <49769313+SchuRich@users.noreply.github.com>
2019-04-21 03:09:11 +0200

    Update Halp V1.00.py

--- Halp V1.00.py
+++ Halp V1.00.py
@@ -8,7 +8,7 @@ async def Halp(ctx):
         "4-> !Staff_Info\n"
         "5-> !Servers_List\n"
         "6-> !Quest_Info **X**\n"
-        "7-> !Brutals_Info **X**\n"
+        "7-> !Brutals_Info\n"
         "8-> !Might_Info **X**\n"
         "9-> !Reborn_Info **X**\n")
 
@@ -72,41 +72,269 @@ async def Servers_Info(ctx):
                    "__Tournaments__ - Nyssa / bloodshed72 , Kaeser\n"
                    "")
 
+
+
+
+
+#####
+
+
+
+
+
 #Seek 6 Quests_Info
 async def Quests_Info(ctx):
     await ctx.send("**__Not Updated__**")
 
-#Seek 7 Brutals_Info
-async def Brutals_Info(ctx):
-    await ctx.send("To see info on brutal u need please use after Brutals_<Brutal Name>")
+
+
+
 
 #####
+
+
+
+
+
+#Seek 7 Brutals_Info
+async def Brutals_Info(ctx):
+    await ctx.send("To see info on brutal u need please use after Brutals_<Brutal Name>\n"
+                   "_ABane\n"
+                   "_BeastOfValley\n"
+                   "_CaveDweller\n"
+                   "_DeathsAdvocate\n"
+                   "_Enigma\n"
+                   "_Fulacks\n"
+                   "_HunterKiller\n"
+                   "_Lerna\n"
+                   "_MotherOfWolfs\n"
+                   "_RatKing\n"
+                   "_RatTree\n"
+                   "_SpiritBros\n"
+                   "_Styx\n"
+                   "_TheSeals\n"
+                   "_TuskTrio\n"
+                   "_Xaph\n")
+
+
 
 #Seek Brutals Data
 async def Brutals_Enigma(ctx):
-    await ctx.send("**__Enigma__**\n"
-                   "__Location:__ Anywhere outside towns and towerns after completed quest.\n\n"
-                   "__Quest:__ Find all 5 Riddles spread throu map and after finishing them all go to eastheaven find \"Riddle Master\" and after gaining ammulet u will be able to find enigma at his location.")
+    await ctx.send("**Enigma**\n\n"
+                   "__Location__: Anywhere outside towns and towerns after completed quest.\n\n"
+                   "__Atk. Type__:\n\n"
+                   "__Skills__:\n\n"
+                   "__Weaknesses__:\n\n"
+                   "__Resistant__:\n\n"
+                   "__Neutral__:\n\n"
+                   "__Quest__: \n"
+                   ">Find all 5 Riddles spread throu map.\n"
+                   "->After finishing them all go to eastheaven and find \"Riddle Master\" \n"
+                   "-->After gaining ammulet from him ENIGMA WILL FIND U.")
+
+async def Brutals_MotherOfWolfs(ctx):
+    await ctx.send("**Mother of Wolves**\n\n"
+                   "__Location__: Forest North of Avan. ( Pic )\n\n"
+                   "__Atk. Type__:\n\n"
+                   "__Skills__:\n\n"
+                   "__Weaknesses__:\n\n"
+                   "__Resistant__:\n\n"
+                   "__Neutral__:\n\n"
+                   "__Quest__:\n"
+                   ">**Complete Master the Hunt Part 1**\n"
+                   "->(speak to Master Hunter Ketevan in town square of AVAN)\n"
+                   "-->Kill 3 wolf beasts and return to him. \n"
+                   "-->Then kill a stalker beasts and return to him.\n"
+                   "---> Then kill Grim Beast. Head due West until the mountain range then proceed South hugging it until you reach cave.\n"
+                   "----> Return to Master Hunter Ketevan and he will sprinkle scent of Waterflower on you to attract Mother of Wolves.\n\n"
+                   "https://i.imgur.com/2gUs9AE.png")
+
+async def Brutals_Styx(ctx):
+    await ctx.send("**Styx's servant**\n\n"
+                   "__Location__: Angel Falls Cemetery\n\n"
+                   "__Atk. Type__: Dark/Lightning\n\n"
+                   "__Skills__:\n\n"
+                   "__Weaknesses__: Blunt\n\n"
+                   "__Resistant__: Dark \n\n"
+                   "__Neutral__: Magic\n\n"
+                   "__Quest__: \n\n"
+                   "https://i.imgur.com/e3JNShV.png")
+
+async def Brutals_Lerna(ctx):
+    await ctx.send("**Lernaean**\n\n"
+                   "__Location__: Dreary Mire ( Pic )\n\n"
+                   "__Atk. Type__:\n\n"
+                   "__Skills__:\n\n"
+                   "__Weaknesses__:\n\n"
+                   "__Resistant__:\n\n"
+                   "__Neutral__:\n\n"
+                   "__Quest__:\n\n"
+                   "https://i.imgur.com/It5GKOV.png")
+
+async def Brutals_TuskTrio(ctx):
+    await ctx.send("**Tusk Trio**\n\n"
+                   "__Location__:\n\n"
+                   "__Atk. Type__:\n\n"
+                   "__Skills__:\n\n"
+                   "__Weaknesses__:\n\n"
+                   "__Resistant__:\n\n"
+                   "__Neutral__:\n\n"
+                   "__Quest__:\n\n"
+                   "https://i.imgur.com/0qq7eTo.png")
+
+async def Brutals_RatKing(ctx):
+    await ctx.send("**RatKing**\n\n"
+                   "__Location__:\n\n"
+                   "__Atk. Type__:\n\n"
+                   "__Skills__:\n\n"
+                   "__Weaknesses__:\n\n"
+                   "__Resistant__:\n\n"
+                   "__Neutral__:\n\n"
+                   "__Quest__:\n"
+                   ">**Complete Master the Hunt Part 3.**\n"
+                   "->speak with Master Hunter Hakan next to blacksmith.\n"
+                   "-->Go to the grassy area above Nyssa's lake and kill 4 lesser skin crawlers.\n"
+                   "---> Go back to him then kill 3 armored trolls in Troldeskoven and return to Haken once more.\n"
+                   "----> Finally go kill The Gruesome Tree Troll in the cave North of the entry into Troldeskoven.\n"
+                   "----->Return to Master Hunter Haken and he'll spray Swamp Snake Skin oil on you to attract. \n\n"
+                   "https://i.imgur.com/oNcMJik.png")
+
+async def Brutals_ABane(ctx):
+    await ctx.send("**Aendisea's Bane**"
+                   "__Location__: Inside ruins of Aendisea. ( Pic )\n "
+                   "__Atk. Type__:\n\n"
+                   "__Skills__:\n\n"
+                   "__Weaknesses__:\n\n"
+                   "__Resistant__:\n\n"
+                   "__Neutral__:\n\n"
+                   "__Quest__: \n"
+                   ">Go to Kamengrad.\n "
+                   "->Head South and cross the river into Yesindale.\n "
+                   "-->Go West and hug the mountain range as you head south.\n "
+                   "--->Once you come across the ruins walk around them and enter from the south end of the ruins lighting the fire to summon Aendisea.\n\n"
+                   "https://i.imgur.com/MShujeY.png")
+
+async def Brutals_SpiritBros(ctx):
+    await ctx.send("**Spirit Bros\n\n"
+                   "__Location__: North part of Vamp Ire, North of Kamengrad (North of University to be precise) in Forest, South of Ways Farm.\n\n"
+                   "__Atk. Type__: Dark\n\n"
+                   "__Skills__:\n\n"
+                   "__Weaknesses__:\n\n"
+                   "__Resists__:\n\n"
+                   "__Neutral__\n\n"
+                   "__Quest__:")
+
+async def Brutals_BeastOfValley(ctx):
+    await ctx.send("**Beast of the Valley**\n\n"
+                   "__Location__: Go to Kamengrad, go south crossing the river to Yesildale. Proceed South hugging the mountains to the east until you come across it's cave.\n\n"
+                   "__Atk. Type__:\n\n"
+                   "__Skills__:\n\n"
+                   "__Weaknesses__:\n\n"
+                   "__Resists__:\n\n"
+                   "__Neutral__:\n\n"
+                   "__Quest__:")
 
 
+async def Brutals_TheSeals(ctx):
+        await ctx.send("**The Seals of Gont**\n\n"
+                       "__Location__: In the forest North/West of Avan.\n\n"
+                       "__Atk. Type__: Blunt and Dark\n\n"
+                       "__Skills__:\n\n"
+                       "__Weaknesses__:\n\n"
+                       "__Resists__:\n\n"
+                       "__Neutral__:\n\n"
+                       "__Quest__:")
 
+async def Brutals_CaveDweller(ctx):
+    await ctx.send("**Kockuburry Cave Dweller**\n\n"
+                   "__Location__: Go to Woodland Market head East to the caves. It's in the 3rd from the top.\n\n"
+                   "__Atk. Type__: Blunt \n\n"
+                   "__Skills__:\n\n"
+                   "__Weaknesses__:\n\n"
+                   "__Resists__:\n\n"
+                   "__Neutral__:\n\n"
+                   "__Quest__:")
 
+async def Brutals_HunterKiller(ctx):
+    await ctx.send("**Hunter Killer**\n\n"
+                   "__Location__: Follow walker of Soils quest. Hunter Killer is in North end to the west./ Or go South from Angel Falls enter the Soils and turn west sorounded by mountains area.\n\n"
+                   "__Atk. Type__: Slashing\n\n"
+                   "__Skills__: Reduce FP10\n\n"
+                   "__Weaknesses__: Blunt\n\n"
+                   "__Resists__:\n\n"
+                   "__Neutral__: Magic\n\n"
+                   "__Quest__:")
 
+async def Brutals_DeathsAdvocate(ctx):
+    await ctx.send("**Deaths Advocate**\n\n"
+                   "__Location__: East part of Easthaven. Hug the East moutain inside of Easthavan from the entrance to Troldeskoven and follow it South into the secluded area where Death's Advocate is located.\n\n"
+                   "__Atk. Type__: Slashing / Piercing / Lightning\n\n"
+                   "__Skills__: Reduce health by 250 points\n\n"
+                   "__Weaknesses__:\n\n"
+                   "__Resists__:\n\n"
+                   "__Neutral__:\n\n"
+                   "__Quest__:")
 
+async def Brutals_Fulacks(ctx):
+    await ctx.send("**Fulacks the Ugly**\n\n"
+                   "__Location__:Enter Troldeskoven through Easthavan and cross either bridge then follow the river North/West to the bridge he resides in.\n\n"
+                   "__Atk. Type__: Blunt\n\n"
+                   "__Skills__:\n\n"
+                   "__Weaknesses__: Blunt \n\n"
+                   "__Resists__:\n\n"
+                   "__Neautral__: Magic\n\n"
+                   "__Quest__:")
 
+async def Brutals_RatTree(ctx):
+    await ctx.send("**Dark Rat Tree**\n\n"
+                   "__Location__: East of Delphi, North of Borewood, south of Ways Farm. Pick one.\n\n"
+                   "__Atk. Type__:\n\n"
+                   "__Skills__:\n\n"
+                   "__Weaknesses__:\n\n"
+                   "__Resists__:\n\n"
+                   "__Neutral__:\n\n"
+                   "__Quest__: \n"
+                   ">Visit Rat tree and speak to it then go kill 20 rats and return to rat tree.")
 
-
-
+async def Brutals_Xaph(ctx):
+    await ctx.send("**Xaphxan The Invisible**\n\n"
+                   "__Location__: Kamengrad Ruins\n\n"
+                   "__Atk. Type__: Dark\n\n"
+                   "__Skills__:\n\n"
+                   "__Weaknesses__:\n\n"
+                   "__Resistant__:\n\n"
+                   "__Neutral__:\n\n"
+                   "__Quest__:\n"
+                   ">**Complete Master the Hunt Part 2.**\n"
+                   "->Speak to Master Hunter Lav next to blacksmith.\n"
+                   "--> Kill 5 lesser spirits and return to him. \n"
+                   "--->Then kill 3 Dark Spirits and return to him. \n"
+                   "---->Then go to The Dreary Mire and kill Dark Spirit Boar. \n"
+                   "----->Lastly return to Master Hunter Lav. He will give you lip balm to attract Xaphan the Invisible.\n\n"
+                   "https://i.imgur.com/ua5FOuZ.png")
 
 
 
 
 
 #####
+
+
+
+
 
 #Seek 8 Mights_Info
 async def Mights_Info(ctx):
     await ctx.send("**__Not Updated__**")
+
+
+
+
+#####
+
+
+
+
 
 #Seek 9 Reborn_Info
 async def Reborn_Info(ctx):
